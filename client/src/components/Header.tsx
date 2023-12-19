0ac1d4359f88cae594635decde60faf95ef00eee
varhin <vanesseaarhin461@gmail.com>
2023-12-19 17:39:02 -0500

    header commponmt added

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+    <h1>Hello welcome to my movie sharing application</h1>
+    <h4>You share all the movies you watched here and for other to aslo watch</h4>
+    <h4>Share the title, comment on the movie and give individual ratings</h4>
+    <h3>#shareYourMoviesWithFriends</h3><hr/><br/>
+    </div>
+  )
+}
+
+export default Header
