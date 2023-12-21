9967ecd025acd9f188ef3be75a3d3b31f55daa69
varhin <vanesseaarhin461@gmail.com>
2023-12-21 10:25:17 -0500

    form app to this page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import './App.css'
 import Home from './components/Home'
+import Header from './components/Header';
+import Form from './components/Form';
 
 function App() {
-  
-  const [favLinks, setFavLinks] = useState([]);
+
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -20,7 +22,7 @@ function App() {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const result = await response.json();
-        setFavLinks(result);
+        setMovies(result);
       } catch (error) {
         console.log(error.message);
       }
@@ -29,7 +31,23 @@ function App() {
   }, []);
   return (
     <>
-      <Home favLinks={favLinks}/>
+      <Header /><br />
+      <div style={{ padding: "10px", display:"flex", flexDirection:"row", gap:"10px", alignItems:"center", justifyContent:"center"}}>
+        <div className="form-group " style={{ padding: "10px"}}>
+          <input type="text" name="genre" className="form-control" id="" placeholder="Search by genre...." required />
+        </div>
+        <div className="form-group " style={{ padding: "10px"}}>
+          <input type="text" name="title" className="form-control" id="" placeholder="Search by title...." required />
+        </div>
+      </div>
+      <div className="container" style={{ width: "100vw", display: "flex", flexDirection: "row", rowGap: 10 }}>
+        <div className="container" style={{ width: "30vw", marginRight: "20px" }}>
+          <Form />
+        </div>
+        <div className="right" style={{ width: "70vw", marginLeft: "10px" }}>
+          <Home movies={movies} />
+        </div>
+      </div>
     </>
   )
 }
