53d379ec0409edba02346a751f9d4bdbdd58ac69
varhin <vanesseaarhin461@gmail.com>
2023-12-22 07:20:19 -0500

    search by genre and reload functionality added.

--- client/src/components/Table.tsx
+++ client/src/components/Table.tsx
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import DataTable from "react-data-table-component";
 
-const Table = ({columns,data}: props) => {
+const Table = ({columns,data}) => {
+  const [searchGenre, setSearchGenre] = useState("");
+  const [tableData, setTableData] = useState(data);
+
+  const handleGenreChange = (event) => { 
+    setSearchGenre(event.target.value) 
+  };
+
+  const handleGenreSearch =()=>{
+    const newRecords = tableData.filter((row)=> row.genre.toLowerCase()==searchGenre.toLowerCase())
+      setTableData(newRecords);
+      
+  }
+
+  const handleReload = async () => {
+    try {
+      const response = await fetch("http://localhost:4000/api/movies", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const result = await response.json();
+      setTableData(result);
+    } catch (error) {
+      console.log(error.message);
+    }  
+  }
+
   return (
-    <DataTable className="table table-striped" columns={columns} data={data} fixedHeader pagination></DataTable>
+    <>
+    <div style={{ padding: "10px", display:"flex", flexDirection:"row", gap:"10px", alignItems:"center", justifyContent:"center"}}>
+        <div className="form-group " style={{ padding: "10px"}}>
+          <input type="text" name="genre" value={searchGenre} onChange={handleGenreChange} className="form-control" id="" placeholder="Search by genre...." required />
+        </div>
+        <div className="form-group " style={{ padding: "10px"}}>
+         <button className="btn btn-primary" onClick={handleGenreSearch}>Search</button>
+         </div>
+         <div className="form-group " style={{ padding: "10px"}}>
+         <button className="btn btn-secondary" onClick={handleReload}>Reload</button>
+         </div>
+      </div>
+      <DataTable className="table table-striped" columns={columns} data={tableData} fixedHeader pagination></DataTable>
+    </>
+   
   );
 };
 
