71bdd0cbc8306daa5c145c7144a06269452ee4c2
varhin <vanesseaarhin461@gmail.com>
2023-12-22 07:19:17 -0500

    typo in sentence fixed

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,9 +4,9 @@ function Header() {
   return (
     <div>
     <h1>Hello welcome to my movie sharing application</h1>
-    <h4>You share all the movies you watched here and for other to aslo watch</h4>
+    <h4>You share all the movies you watched here for other to aslo download and watch</h4>
     <h4>Share the title, comment on the movie and give individual ratings</h4>
-    <h3>#shareYourMoviesWithFriends</h3><hr/><br/>
+    <h3>#shareYourWatchedMoviesWithFriends</h3><hr/><br/>
     </div>
   )
 }
